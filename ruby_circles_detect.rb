724119cdbb36565ea4900a35923f08a1df53cb09
Joseph Combs <joseph.e.combs@gmail.com>
2014-10-17 22:57:10 -0700

    I think this might work

--- ruby_circles_detect.rb
+++ ruby_circles_detect.rb
@@ -4,13 +4,7 @@ require "rubygems"
 require "opencv"
 include OpenCV
 
-original_window = GUI::Window.new("original", flags = CV_WINDOW_NORMAL)
-hough_window = GUI::Window.new "hough circles"
-
-image = IplImage::load "test2.jpg"
-gray = image.BGR2GRAY
-# gray = gray.resize(1200,800)
-
+#gross window resizing code
 class MySize
   def width
     1200
@@ -23,24 +17,39 @@ end
 mp = MySize.new
 CvSize.compatible?(mp)  #=> true
 aaa = CvSize.new(mp)          #=> same as CvSize(10, 20)
-puts aaa
-# gray.resize(aaa, interpolation = :linear)
+
+original_window = GUI::Window.new("original", flags = CV_WINDOW_NORMAL)
+hough_window = GUI::Window.new "hough circles"
+
+image = IplImage::load "test2.jpg"
+gray = image.BGR2GRAY
+# gray = gray.resize(1200,800)
+
+detect = gray.hough_circles(CV_HOUGH_GRADIENT, 2.0, 10, 200, 10)
+puts detect.size
+detect.each{|circle|
+  puts "#{circle.center.x},#{circle.center.y} - #{circle.radius}"
+  result.circle! circle.center, circle.radius, :color => CvColor::Red, :thickness => 3
+}
+
+result = result.resize(aaa)
+
+hough_window.show result
+
+
+
+
+
+
 
 #do this at the very end, ultimately
-gray = gray.resize(aaa)
+# gray = gray.resize(aaa)
 # gray = gray.resize(aaa)
 # gray.resize(mp, interpolation = :linear)
 
 # original_window.show image
 
-original_window.show gray
+# original_window.show gray
 
 
-# detect = image.hough_circles(CV_HOUGH_GRADIENT, 2.0, 10, 200, 10)
-# puts detect.size
-# detect.each{|circle|
-#   puts "#{circle.center.x},#{circle.center.y} - #{circle.radius}"
-#   result.circle! circle.center, circle.radius, :color => CvColor::Red, :thickness => 3
-# }
-# hough_window.show result
 GUI::wait_key
