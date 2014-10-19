5334ce472cc0b695b41d0933166fa2efa2c94044
Joseph Combs <joseph.e.combs@gmail.com>
2014-10-18 23:29:23 -0700

    trying some new stuff

--- ruby_circles_detect.rb
+++ ruby_circles_detect.rb
@@ -21,33 +21,45 @@ aaa = CvSize.new(mp)          #=> same as CvSize(10, 20)
 original_window = GUI::Window.new("original", flags = CV_WINDOW_NORMAL)
 hough_window = GUI::Window.new "hough circles"
 
+# image = IplImage::load "test4.jpg"
 image = IplImage::load "test2.jpg"
 gray = image.BGR2GRAY
 # gray = gray.resize(1200,800)
-gray = gray.canny(1100, 40)
+gray = gray.canny(1100, 100)
 # hough_window.show gray
 
 # sleep 100
 #
-# detect = gray.hough_circles(CV_HOUGH_GRADIENT, 1, 20, 50, 30, 100)
+#pass through once...
+#the original image is probably too detailed so we need to filter it a bit to get our circles
+# gray = gray.smooth(CV_GAUSSIAN,3,3)
+# gray = gray.smooth(CV_BLUR,17,20)
+
+# see here http://stackoverflow.com/questions/23976766/hough-circle-detection-accuracy-very-low
+# usage:
+# hough_circles(method, dp, min_dist, param1, param2, min_radius = 0, max_radius = 0)
+detect = gray.hough_circles(CV_HOUGH_GRADIENT, 1,100, 1, 80, 10)
+# detect = gray.hough_circles(CV_HOUGH_GRADIENT, 2, 160, 50, 30)
 puts gray.rows
 
 
-# detect.each{|circle|
-#   puts "#{circle.center.x},#{circle.center.y} - #{circle.radius}"
-#   gray.circle! circle.center, circle.radius, :color => CvColor::Red, :thickness => 3
-# }
+detect.each{|circle|
+   puts "#{circle.center.x},#{circle.center.y} - #{circle.radius}"
+   image.circle! circle.center, circle.radius, :color => CvColor::Red, :thickness => 3
+}
 
-gray.circle!(CvPoint.new(gray.width / 2, gray.height / 2), 800,
-               :color => CvColor::Blue, :thickness => 1, :line_type => :aa)
+# gray.circle!(CvPoint.new(gray.width / 2, gray.height / 2), 800, :color => CvColor::Blue, :thickness => 1, :line_type => :aa)
 
 # result = result.resize(aaa)
 gray = gray.resize(aaa)
 
-gray.circle! OpenCV::CvPoint.new(500,500), 500, color: CvColor::Red, thickness: 3
+
 
 hough_window.show gray
 # hough_window.show gray
+# image = image.smooth(CV_BLUR,10,10)
+image = image.resize(aaa)
+original_window.show image
 
 
 
